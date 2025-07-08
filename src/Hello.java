e86d0e7b57e19c8b561ab36d314fd2935ea10b74
gurleenkaur610 <gurleenku610@gmail.com>
2025-07-09 00:05:48 +0530

    my first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String [] args){
+        System.out.println("Hello Tim!!");
+
+        boolean isAlien = true;
+        if(isAlien == false){
+            System.out.println("It is not an ALien!");
+            System.out.print("And I am scared of the Aliens");
+            }
+    }
+}
